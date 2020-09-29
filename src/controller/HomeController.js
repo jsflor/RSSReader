6349a5152ecbf650802dc1470e299acbca3f2f41
Sebastian Flor <sflor@telecomboutique.com>
2020-09-29 22:01:23 +0200

    Added stack navigation between HomeScreen.js and DetailScreen.js in HomeController.js

--- src/controller/HomeController.js
+++ src/controller/HomeController.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import HomeView from '../view/Home/HomeView';
+import HomeScreen from '../view/Home/HomeScreen';
+import DetailScreen from '../view/Home/DetailScreen';
 
-const HomeController = () => <HomeView />;
+const HomeController = () => {
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name={'Home'} component={HomeScreen} />
+        <Stack.Screen name={'Detail'} component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default HomeController;
